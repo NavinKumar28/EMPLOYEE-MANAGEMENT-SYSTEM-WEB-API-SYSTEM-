1f9165ae28207f6cca03d5ce5c7fad9a663e80f3
NAVIN KUMAR <87271759+NavinKumar28@users.noreply.github.com>
2024-02-10 18:28:39 +0530

    Add files via upload
    DATA

---
+++ EMPLOYEEE MANAGEMENT DATA.sql
@@ -0,0 +1,14 @@
+INSERT INTO Employee (EmpCode, DOB, EmpName, EmailAddress, DeptCode)
+VALUES
+    (301, '2023-01-01 10:00:00', 'Amit Patel', 'amit.patel@gmail.com', 103),
+    (302, '2023-01-02 10:00:00', 'Rajesh Sharma', 'rajesh.sharma@gmail.com', 102),
+    (303, '2023-01-03 10:00:00', 'Deepika Singh', 'deepika.singh@gmail.com', 105),
+    (304, '2023-01-04 10:00:00', 'Neha Gupta', 'neha.gupta@gmail.com', 101),
+    (305, '2023-01-05 10:00:00', 'Sanjay Kumar', 'sanjay.kumar@gmail.com', 107),
+    (306, '2023-01-06 10:00:00', 'Priya Shah', 'priya.shah@gmail.com', 108),
+    (307, '2023-01-07 10:00:00', 'Vivek Mishra', 'vivek.mishra@gmail.com', 103),
+    (308, '2023-01-08 10:00:00', 'Pooja Mehta', 'pooja.mehta@gmail.com', 108),
+    (309, '2023-01-09 10:00:00', 'Rohit Singh', 'rohit.singh@gmail.com', 101),
+    (310, '2023-01-10 10:00:00', 'Anjali Sharma', 'anjali.sharma@gmail.com', 102);
+
+	select*from Employee
